3618655fa34197bf9e091af6c4e6b2b36419529e
Michael Wilson <mike.wilson@datadoghq.com>
2021-05-21 11:35:42 -0400

    Rename QueryRO to ROQuery.

--- client_test.go
+++ client_test.go
@@ -68,9 +68,9 @@ func TestMatchQuery(t *testing.T) {
 	checkQueryResults(t, res)
 }
 
-func TestMatchQueryRO(t *testing.T) {
+func TestMatchROQuery(t *testing.T) {
 	q := "MATCH (s)-[e]->(d) RETURN s,e,d"
-	res, err := graph.QueryRO(q)
+	res, err := graph.ROQuery(q)
 	if err != nil {
 		t.Error(err)
 	}
@@ -134,9 +134,9 @@ func TestCreateQuery(t *testing.T) {
 	assert.Equal(t, w.Label, "WorkPlace", "Unexpected node label.")
 }
 
-func TestCreateQueryROFailure(t *testing.T) {
+func TestCreateROQueryFailure(t *testing.T) {
 	q := "CREATE (w:WorkPlace {name:'RedisLabs'})"
-	_, err := graph.QueryRO(q)
+	_, err := graph.ROQuery(q)
 	assert.NotNil(t, err, "error should not be nil")
 }
 
