2ce2bc293595b62e89c3c1fd1cab00ce3669155e
Avital Fine <79420960+AvitalFineRedis@users.noreply.github.com>
2021-05-30 16:06:42 +0300

    Update client_test.go

--- client_test.go
+++ client_test.go
@@ -408,21 +408,21 @@ func TestUtils(t *testing.T) {
 	res := RandomString(10)
 	assert.Equal(t, len(res), 10)
 
-    res = ToString("test_string")
-    assert.Equal(t, res, "\"test_string\"")
+	res = ToString("test_string")
+	assert.Equal(t, res, "\"test_string\"")
+	
+	res = ToString(10)
+	assert.Equal(t, res, "10")	
 
-    res = ToString(10)
-    assert.Equal(t, res, "10")	
-
-    res = ToString(1.2)
-    assert.Equal(t, res, "1.2")
+	res = ToString(1.2)
+	assert.Equal(t, res, "1.2")
 
 	res = ToString(true)
-    assert.Equal(t, res, "true")
+	assert.Equal(t, res, "true")
 
 	var arr = []interface{}{1,2,3,"boom"}
 	res = ToString(arr)
-    assert.Equal(t, res, "[1,2,3,\"boom\"]")
+	assert.Equal(t, res, "[1,2,3,\"boom\"]")
 	
 	jsonMap := make(map[string]interface{})
 	jsonMap["object"] = map[string]interface{} {"foo": 1}
