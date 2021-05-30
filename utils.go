1996ab36ef73750aef09b6259bdc5f0b00d33193
AvitalFineRedis <avital.fine@redislabs.com>
2021-05-30 13:44:16 +0300

    support map conversion and test utils file

--- utils.go
+++ utils.go
@@ -18,6 +18,14 @@ func arrayToString(arr []interface{}) string {
 	return "[" + strings.Join(strArray, ",") + "]"
 }
 
+func mapToString(data map[string]interface{}) string {
+	pairsArray := []string{}
+	for k, v := range data {
+		pairsArray = append(pairsArray, k + ": " + ToString(v))
+	}
+	return "{" + strings.Join(pairsArray, ",") + "}"
+}
+
 func ToString(i interface{}) string {
 	if(i == nil) {
 		return "null"
@@ -33,9 +41,12 @@ func ToString(i interface{}) string {
 		return strconv.FormatFloat(i.(float64), 'f', -1, 64)
 	case bool:
 		return strconv.FormatBool(i.(bool))
-	case []interface {}:
+	case []interface{}:
 		arr := i.([]interface{})
 		return arrayToString(arr)
+	case map[string]interface{}:
+		data := i.(map[string]interface{})
+		return mapToString(data)
 	default:
 		panic("Unrecognized type to convert to string")
 	}
